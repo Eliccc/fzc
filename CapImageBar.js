81217dab70b91052bafad938e5c9f88f20f7a0c0
李仙探 <2468793043@qq.com>
2017-04-24 01:14:16 +0800

    Update CapImageBar.js
    second

--- CapImageBar.js
+++ CapImageBar.js
@@ -79,9 +79,10 @@ CapImageBar.prototype.addLongtap = function(elementId, _this){
 	var timer;
 	var target = document.getElementById(elementId);
 	var rmEle = document.createElement("DIV");
+	
 	rmEle.id = elementId+'rm';
 	rmEle.className = 'rm';
-	var rmEnable = false;
+	rmEle.innerHTML = 'X';
 
 
 //	file.onchange=function(){
@@ -91,17 +92,24 @@ CapImageBar.prototype.addLongtap = function(elementId, _this){
 //	}
 
 
-	rmEle.onclick = function(){
+	rmEle.onclick = function(e){
+		console.log('=============')
 		target.removeChild(rmEle);
 //		div1.style.display = 'none'    //这个是删除div1
 		//更换背景图
 		console.log(target.id);
 		target.childNodes[0].src = "./image/image0.png";
+		console.log(_this.rmEnable)
+		e.preventDefault();
+		e.stopPropagation();
 	}
+
 	var longtap = function(){
 		timer = setTimeout(function(){
 			target.appendChild(rmEle);
-			rmEnable = true;
+			_this.rmEnable = true;
+			_this.tmp = target;
+			_this.rmId = rmEle.id;
 			console.log('set true');
 		},1000);
 	
@@ -119,33 +127,78 @@ CapImageBar.prototype.addLongtap = function(elementId, _this){
 	target.ontouchend = stoptap;
 
 	target.onclick = function(e){
-		console.log(rmEnable);
-		if(!rmEnable){
-//			_this.tmp = target;
+		console.log(_this.rmEnable);
+		if(!_this.rmEnable){
+			_this.tmp = target;
 //			file.click();
+			console.log('single click');
 			
-			rmEnable = false;
+			var floater = document.createElement("DIV");
+			floater.className = 'floater';
+			var btn1 = document.createElement("DIV");
+			btn1.className = 'strangeDiv';
+			btn1.innerHTML = 'btn1';
+			var btn2 = document.createElement("DIV");
+			btn2.className = 'strangeDiv';
+			btn2.innerHTML = 'btn2';
+			floater.appendChild(btn1);
+			floater.appendChild(btn2);
+			document.body.appendChild(floater);
+			_this.floaterEnable = true;
+			btn1.onclick = function(){
+				console.log('btn1 '+ elementId)
+				target.childNodes[0].src = "./image/image3.png";
+			}
+
+			btn2.onclick = function(){
+				console.log('btn2 '+ elementId)
+					target.childNodes[0].src = "./image/image4.png";
+			}
+
+			_this.rmEnable = false;
 		}
 	}
 
 //
 //	//点击其他地方的时候，rm隐藏
-//	document.body.onmousedown = function(){
-//		var e = e || window.event; 
-//		var elem = e.target || e.srcElement;  
-//		while (elem) {
-//			if (elem.id && elem.id == elementId+'rm') {  
-//				return;  
-//			}
-//			elem = elem.parentNode;  
-//		}
-//		console.log('test able:'+rmEnable);
-//			document.removeChild(rmEle);
-//			rmEnable = false;
-//	}
+	document.body.onmousedown = function(){
+		var e = e || window.event; 
+		var elem = e.target || e.srcElement;  
+		while (elem) {
+			if (elem.className == 'strangeDiv') {  
+				return;  
+			}
+			
+			if(elem.id && elem.id!="" && elem.id==(_this.rmId)){
+				_this.rmEnable = false;
+				return;
+			}
+			elem = elem.parentNode;  
+		}
+		console.log('test able:'+_this.rmEnable);
+		if(_this.rmEnable && _this.tmp){
+			var rm = document.getElementById(_this.rmId);
+			_this.tmp.removeChild(rm)
+		};
+		if( _this.floaterEnable){
+			var floaters = document.getElementsByClassName('floater');
+			for(var i = 0; i < floaters.length; i++){
+				i
+				document.body.removeChild(floaters[i]);
+			}
+		}
+		_this.rmEnable = false;
+		_this.floaterEnable = false;
+		console.log(_this.rmEnable)
+	}
 }
 
+
+
 CapImageBar.prototype.tmp;
+CapImageBar.prototype.rmEnable = false;
+CapImageBar.prototype.floaterEnable = false;
+CapImageBar.prototype.rmId;
 
 CapImageBar.prototype.setImages = function(arr){
 	this.capImageBar.imageList = arr;
